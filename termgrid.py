d1e68dcb94fa82d4642a7ccb4d654b62507dba79
Medansh <just.medansh@gmail.com>
2026-05-12 15:30:54 +0530

    Added validation and custom mode

--- termgrid.py
+++ termgrid.py
@@ -7,17 +7,17 @@ class Grid:
         1: "red",
     }
 
-    def __init__(self, rows=None, cols=None, mode="fit"):
-        if mode != "fit" and (rows == None or cols == None):
-            raise ValueError("Specify rows and columns if mode is custom")
-        #TODO: ADD CUSTOM MODE
+    def __init__(self, rows=80, cols=40, mode="fit"):
+        if mode not in ['fit', 'custom']:
+            raise ValueError(f"Mode must be 'custom' or 'fit', '{mode}' is not a valid mode")
+        if not self.are_positive_ints(rows, cols):
+            raise ValueError("rows and cols must be a positive integer")
         if mode == "fit":
-            self.cols, self.rows = shutil.get_terminal_size((40, 80))
-        else:
+            self.cols, self.rows = shutil.get_terminal_size((cols, rows))
+        elif mode == "custom":
             self.cols = cols
             self.rows = rows
-        #TODO: ADD VALIDATION
-        self.grid = self.gen_grid(self.rows, self.cols)
+        self.grid = self.gen_blank_grid(self.rows, self.cols)
 
     def __str__(self):
         lines = []
@@ -34,10 +34,17 @@ class Grid:
             #TODO: ADD SPACE BETWEEN LETTERS AS CHOICE
         return "\n".join(lines)
 
+    @staticmethod
+    def are_positive_ints(*values):
+        for value in values:
+            if not isinstance(value, int) or isinstance(value, bool) or value < 1:
+                return False
+        return True        
+
     def gen_blank_grid(self, rows, cols):
         res = []
         for _ in range(rows):
-            res.append([' '] * cols)
+            res.append([' '] * cols) 
         return res
     
     def get_cell(self, pos):
